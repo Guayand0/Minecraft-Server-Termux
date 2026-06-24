374c1d94722d47de6b9d3ce884246d385910f0a4
David Beamonde <179389440+Guayand0@users.noreply.github.com>
2026-06-24 17:41:54 +0200

    Allow diferent MC versions with link request

--- 1_install-Debian.sh
+++ 1_install-Debian.sh
@@ -1,12 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+set -e
+
 yes | pkg update
 yes | pkg upgrade
 yes | pkg install proot-distro -y
 
 proot-distro install debian
 
-# crear script de acceso
+# crear script de accesoa Debian
 cat << 'EOF' > 1_debian.sh
 #!/data/data/com.termux/files/usr/bin/bash
 proot-distro login debian
@@ -16,10 +18,9 @@ chmod +x 1_debian.sh
 
 # Crear README
 cat << 'EOF' > README.txt
-Para iniciar debian usa "./1_debian.sh", una vez dentro usa "./1_start.sh" para iniciar el servidor
+Para iniciar Debian usa "./1_debian.sh", una vez dentro usa "./1_start.sh" para iniciar el servidor de MC
 EOF
 
-echo "Instalación completa. Entrando en debian con ./1_debian.sh"
-echo "usa 'curl -fsSL https://raw.githubusercontent.com/Guayand0/Minecraft-Server-Termux/main/2_install-MC.sh | bash' para terminar la instalacion de MC"
-
-./1_debian.sh
+echo "Debian instalado correctamente."
+echo "Usa './1_debian.sh' para entrar a Debian"
+echo "Usa 'curl -fsSL https://raw.githubusercontent.com/Guayand0/Minecraft-Server-Termux/main/2_install-MC.sh | bash' para terminar la instalacion del servidor de MC"
--- 2_install-MC.sh
+++ 2_install-MC.sh
@@ -1,10 +1,26 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
+set -e
+
 apt update -y
 apt upgrade -y
 apt install -y openjdk-25-jdk
 
-curl -L -o server.jar https://piston-data.mojang.com/v1/objects/823e2250d24b3ddac457a60c92a6a941943fcd6a/server.jar
+# URL del server.jar (argumento 1)
+MC_URL="$1"
+
+if [ -z "$MC_URL" ]; then
+  echo "Uso: $0 <URL_server.jar>"
+  echo "Ejemplos:"
+  echo "$0 https://piston-data.mojang.com/v1/objects/823e2250d24b3ddac457a60c92a6a941943fcd6a/server.jar"
+  echo "$0 https://fill-data.papermc.io/v1/objects/ebbce8dcd115170c234af6d132771282ad89b7df410f03ada503d8c32c8fd5ad/paper-26.2-34.jar"
+  exit 1
+fi
+
+echo "Descargando server.jar desde:"
+echo "$MC_URL"
+
+curl -L -o server.jar "$MC_URL"
 
 # Crear 1_start.sh
 cat << 'EOF' > 1_start.sh
